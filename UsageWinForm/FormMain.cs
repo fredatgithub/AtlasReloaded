650acfed2d79f0d3ddd3186f5c96269604c2f99f
Freddy Juhel <domaine75@gmail.com>
2015-10-20 10:26:42 +0200

    Fix utf-8 without space

--- UsageWinForm/FormMain.cs
+++ UsageWinForm/FormMain.cs
@@ -126,11 +126,7 @@ namespace UsageWinForm
     {
       List<string> minimumVersion = new List<string>
       {
-        "<?xml version=\"1.0\" encoding=\"utf - 8\" ?>",
-        "<Document>",
-        "<DocumentVersion>",
-        "<version> 1.0 </version>",
-        "</DocumentVersion>",
+        "<?xml version=\"1.0\" encoding=\"utf-8\" ?>",
         "<terms>",
          "<term>",
         "<name>MenuFile</name>",
@@ -262,8 +258,7 @@ namespace UsageWinForm
       "<englishValue>About</englishValue>",
       "<frenchValue>A propos de ...</frenchValue>",
     "</term>",
-  "</terms>",
-"</Document>"
+  "</terms>"
       };
       StreamWriter sw = new StreamWriter(Settings.Default.LanguageFileName);
       foreach (string item in minimumVersion)
